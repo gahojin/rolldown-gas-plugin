b56e9c39c8ea8e62ac389821b69f65287ca898cb
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2025-07-30 15:47:42 +0900

    test: rolldownの修正に伴うテスト修正
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- src/index.test.ts
+++ src/index.test.ts
@@ -125,7 +125,7 @@ this.main3 = a.main3;`)
     expect(outfile).toEqual(`function main1() {}
 function main2() {}
 function main3() {}
-var _=function(exports){let t=e=>e,n=()=>{console.log(t(\`hello world.\`))},r=(e,t)=>e+t,i=(e,t)=>e-t,a=()=>{console.log(t(\`1 + 2 = \${r(1,2)}\`))},o=()=>{console.log(t(\`3 - 1 = \${i(3,1)}\`))};return exports.main1=a,exports.main2=o,exports.main3=n,exports}({});
+var _=(function(exports){let t=e=>e,n=()=>{console.log(t(\`hello world.\`))},r=(e,t)=>e+t,i=(e,t)=>e-t,a=()=>{console.log(t(\`1 + 2 = \${r(1,2)}\`))},o=()=>{console.log(t(\`3 - 1 = \${i(3,1)}\`))};return exports.main1=a,exports.main2=o,exports.main3=n,exports})({});
 this.main1 = _.main1;
 this.main2 = _.main2;
 this.main3 = _.main3;`)
@@ -149,7 +149,7 @@ this.main3 = _.main3;`)
     expect(outfile).toEqual(`function main1() {}
 function main2() {}
 function main3() {}
-var a=function(exports){let t=e=>e,n=()=>{console.log(t(\`hello world.\`))},r=(e,t)=>e+t,i=(e,t)=>e-t,o=()=>{console.log(t(\`1 + 2 = \${r(1,2)}\`))},s=()=>{console.log(t(\`3 - 1 = \${i(3,1)}\`))};return exports.main1=o,exports.main2=s,exports.main3=n,exports}({});
+var a=(function(exports){let t=e=>e,n=()=>{console.log(t(\`hello world.\`))},r=(e,t)=>e+t,i=(e,t)=>e-t,o=()=>{console.log(t(\`1 + 2 = \${r(1,2)}\`))},s=()=>{console.log(t(\`3 - 1 = \${i(3,1)}\`))};return exports.main1=o,exports.main2=s,exports.main3=n,exports})({});
 this.main1 = a.main1;
 this.main2 = a.main2;
 this.main3 = a.main3;`)
