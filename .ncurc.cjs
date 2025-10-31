3903bd8fd25ea026240bf43c470adb737aef1963
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2025-10-31 13:17:15 +0900

    build(deps): fast-checkのvitest依存警告を抑止
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -2,7 +2,7 @@ const { defineConfig } = require('npm-check-updates')
 
 module.exports = defineConfig({
   target: (name) => {
-    if (name === '@types/node' || name === 'vitest' || name.startsWith('@vitest/')) {
+    if (name === '@types/node') {
       return 'minor'
     }
     return 'newest'
