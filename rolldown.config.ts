72924a59b5eb48099cc949b985309acb13d8655a
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2026-01-22 08:40:52 +0900

    build!: cjsの出力を廃止し、esmのみに変更
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -6,10 +6,7 @@ export default defineConfig([
     treeshake: true,
     input: 'src/index.ts',
     platform: 'node',
-    output: [
-      { dir: 'dist', format: 'esm', entryFileNames: '[name].mjs', sourcemap: true, cleanDir: true },
-      { dir: 'dist', format: 'cjs', entryFileNames: '[name].cjs', sourcemap: true, exports: 'named' },
-    ],
+    output: [{ dir: 'dist', format: 'esm', entryFileNames: '[name].mjs', sourcemap: true, cleanDir: true }],
     plugins: [IsolatedDecl()],
   },
 ])
